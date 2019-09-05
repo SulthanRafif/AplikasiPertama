a82a233cc6d25afa05d7c9002987fbaf069e2d63
SulthanRafif <rsulthan5@gmail.com>
2019-09-05 14:16:29 +0700

    on start and on stop

--- app/src/main/java/com/example/aplikasipertama/MainActivity.java
+++ app/src/main/java/com/example/aplikasipertama/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.aplikasipertama;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "App on start", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "App on stop", Toast.LENGTH_SHORT).show();
     }
 }
